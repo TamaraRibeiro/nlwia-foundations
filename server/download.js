d9db6f66e1be6495c97025f2e30a6dcccf21bae2
TamaraRibeiro <tamara.fl@hotmail.com>
2023-09-13 21:31:46 -0400

    Frontend fixes; start backend

---
+++ server/download.js
@@ -0,0 +1,22 @@
+import ytdl from "ytdl-core"
+import fs from "fs"
+
+export const download = (videoId) => {
+  const videoURL = "https://www.youtube.com/shorts/" + videoId
+  console.log("Downloading video: ", videoId)
+
+  ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" }).on(
+    "info",
+    (info) => {
+      const seconds = info.formats[0].approxDurationMs / 1000
+      
+      if(seconds > 60) {
+        throw new Error("The video duration is longer than 60 seconds")
+      }
+    }
+  ).on("end", () => {
+    console.log("Download finished.")
+  }).on("error", (error) => {
+    console.log("Unable to download video. Error details: ", error)
+  }).pipe(fs.createWriteStream("./tmp/audio.mp4"))
+}
